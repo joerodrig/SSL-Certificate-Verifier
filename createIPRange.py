26cca7ded21f0808c537da4f88468b334e085e46
Joseph Rodriguez <joerodrig3@gmail.com>
2014-05-23 02:25:38 -0400

    Update createIPRange.py

--- createIPRange.py
+++ createIPRange.py
@@ -1,6 +1,9 @@
 '''
+Author: Joseph Rodriguez
+
+Last Edit: May 23,2014
 Credits to: http://cmikavac.net/2011/09/11/how-to-generate-an-ip-range-list-in-python/
-For the initial code that I modified a bit for my purposes
+For the initial ipRange() code that I modified slightly for my purposes
 
 Use this program to create an IP Range List to read from.
 
@@ -10,30 +13,32 @@ The text file created is called: IPRange.txt
 
 '''
 def main():
-    global IP_list
     IP_list = []
-    
+    getRange(IP_list)
+
+
+def getRange(ipl):
     print 'Enter a range(ex. From: 173.252.1.0 , To: 173.252.1.200)'
     starting_IP = raw_input('From: ')
     ending_IP = raw_input('To: ')
-    ipRange(starting_IP,ending_IP)
+    ipRange(ipl,starting_IP,ending_IP)
 
-    addMore = raw_input('Do you want to add another IP range?Y/N')
-    addMore = addMore[0].lower()
-    if addMore == 'y':
-        getRanges()
+    #Add more addresses or write addresses to IPrange.txt file
+    getRange(ipl) if raw_input('Add another IP range?y/n')[0].lower() == 'y' else writeToText(ipl)
 
-    else:
-        rangeText = open('IPrange.txt','w')
-        for i in range(len(IP_list)):
-            rangeText.write(IP_list[i] + '\n')
-        rangeText.close()
-def ipRange(start_ip, end_ip):
+def writeToText(ipl_in):
+    ranges = open('IPrange.txt','w')
+    for i in range(len(ipl_in)):
+        ranges.write(ipl_in[i] + '\n')
+    ranges.close()
+    print 'Wrote to addresses IPrange.txt'
+
+def ipRange(IP_List,start_ip, end_ip):
     start = list(map(int, start_ip.split(".")))
     end = list(map(int, end_ip.split(".")))
     temp = start
     
-    IP_list.append(start_ip)
+    IP_List.append(start_ip)
     while temp != end:
         start[3] += 1
         for i in (3, 2, 1):
@@ -41,5 +46,5 @@ def ipRange(start_ip, end_ip):
                 temp[i] = 0
                 temp[i-1] += 1        
         IP = ".".join(map(str, temp))
-        IP_list.append(IP)
+        IP_List.append(IP)
 main()
