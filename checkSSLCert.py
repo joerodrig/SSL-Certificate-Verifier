0509b66f8bde812d8d6bb12cb9890eebcd1a325c
Joseph Rodriguez <joerodrig3@gmail.com>
2014-05-24 01:43:23 -0400

    Removed all global vars
    Removed all global vars through class implementation

--- checkSSLCert.py
+++ checkSSLCert.py
@@ -1,7 +1,7 @@
 '''
 Author: Joseph Rodriguez
-Date: August 1st 2013
-Latest Update: May 23,2014
+Creation Date: August 1st 2013
+Latest Update: May 24,2014
 Description:
 This script will search through all of the IP addresses(5 at a time) provided upon startup, and will grab certificate
 information from each IP address and write it to a spreadsheet file(.csv).
@@ -17,33 +17,116 @@ import re
 from datetime import datetime
 
 def main():
-    now = datetime.now()
-    global title
-    title = now.strftime("%Y-%m-%d_%H-%M-%S") + '_Log.csv'
-    timeLog = TimeLog(now,title) # Track start time
+
+    ssl_file = SSL_Sheet()
+
+    timeLog = TimeLog(ssl_file.getTitle()) # Track start time
     
-    c = open(title, 'w')
-    w = csv.writer(c)
-    w.writerow(['IP', 'Company Name', 'ExpirationDate','Time Left(In Days)', 'CommonName', 'Issuer',
-                'Serial #','Public Key','Public Key Size','Error Code','Description'])
-    c.close()
+    ssl_file.write_CSV_Header()
 
+    ssl_file.getRanges(timeLog)
 
-    global IP_list
-    IP_list = []
-    global certInfoHolder
-    certInfoHolder = []
-    global brokenCerts
-    brokenCerts = []
-
-    getRanges(timeLog)
+    ssl_file.close_CSV()
 
     timeLog.logEnd(str(datetime.now())) # Write end time to run log
 
+class SSL_Sheet:
+    def __init__(self):
+        self.title = self.createFile()
+        self.open_sheet = open(self.title,'a')
+        self.IP_list = []
+        self.brokenCerts = []
+
+    def createFile(self):
+        return datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '_Log.csv'
+
+    def write_CSV_Header(self):
+        w = csv.writer(self.open_sheet)
+        w.writerow(['IP', 'Company Name', 'ExpirationDate','Time Left(In Days)', 'CommonName', 'Issuer',
+                'Serial #','Public Key','Public Key Size','Error Code','Description'])
+
+    def getTitle(self):
+        return self.title
+
+    def ipRange(self,start_ip, end_ip):
+        start = list(map(int, start_ip.split(".")))
+        end = list(map(int, end_ip.split(".")))
+        temp = start
+        
+        self.IP_list.append(start_ip)
+        while temp != end:
+            start[3] += 1
+            for i in (3, 2, 1):
+                if temp[i] == 256:
+                    temp[i] = 0
+                    temp[i-1] += 1        
+            IP = ".".join(map(str, temp))
+            self.IP_list.append(IP)
+    
+    def writeToCSV(self,info,bad_cert):
+        w = csv.writer(self.open_sheet)
+        if bad_cert == False:
+            w.writerow([info[0], info[1], info[2], info[3], info[4] , info[5][16:],
+                                     info[6],info[7],info[8],0])
+        else:
+            w.writerow([info[0],'','','','','','','','',info[1],info[2]])
+    
+    def close_CSV(self):
+        self.open_sheet.close()
+
+    def getRanges(self,tl):
+        print 'Enter a range(ex. From: 173.252.1.0 , To: 173.252.1.200)'
+        starting_IP = raw_input('From: ')
+        ending_IP = raw_input('To: ')
+
+        ip_ranges = self.ipRange(starting_IP, ending_IP)
+        addMore = raw_input('Do you want to add another IP range?Y/N')
+        if addMore[0].lower() == 'y':
+            getRanges(tl)       
+        elif addMore[0].lower() == 'n':
+            tl.amtToScan(len(self.IP_list)) #Write number of IPs being scanned to run log
+            createdThreads = 0
+            threadSplit = len(self.IP_list) / 5
+
+            #Splitting the work up between the threads
+            thread_1_list = self.IP_list[0:threadSplit]
+            thread_2_list = self.IP_list[threadSplit:(threadSplit*2)]
+            thread_3_list = self.IP_list[(threadSplit*2):(threadSplit*3)]
+            thread_4_list = self.IP_list[(threadSplit*3):(threadSplit*4)]
+            thread_5_list = self.IP_list[(threadSplit*4):(threadSplit*5)]
+            thread_6_list = self.IP_list[(threadSplit*5):]
+
+            threadList = [] 
+            for address in range(threadSplit):
+                thread_1 = getCertInfo(thread_1_list[address],self)
+                thread_2 = getCertInfo(thread_2_list[address],self)
+                thread_3 = getCertInfo(thread_3_list[address],self)
+                thread_4 = getCertInfo(thread_4_list[address],self)
+                thread_5 = getCertInfo(thread_5_list[address],self)
+                thread_1.start()
+                thread_2.start()
+                thread_3.start()
+                thread_4.start()
+                thread_5.start()
+        
+                thread_1.join()      
+                thread_2.join()
+                thread_3.join()
+                thread_4.join()
+                thread_5.join()
+         
+                if address == threadSplit-1:
+                    for address in range(len(thread_6_list)):
+                         thread_6 = getCertInfo(thread_6_list[address],self)
+                         thread_6.start()
+                         thread_6.join()
+                
+
+
 class TimeLog:
-    def __init__(self,cur_time,title):
+    def __init__(self,title):
         self.tl = open('run_logs.txt','a')
-        self.logStart(cur_time,title)
+        self.logStart(datetime.now(),title)
 
     def logStart(self,c_time,csv_title):
         self.tl.write('Time Started: {0}\n'.format(str(c_time)))
@@ -57,83 +140,13 @@ class TimeLog:
         self.tl.close()
 
 
-def getRanges(tl):
-    print 'Enter a range(ex. From: 173.252.1.0 , To: 173.252.1.200)'
-    starting_IP = raw_input('From: ')
-    ending_IP = raw_input('To: ')
-
-    ip_ranges = ipRange(starting_IP, ending_IP)
-    addMore = raw_input('Do you want to add another IP range?Y/N')
-    if addMore[0].lower() == 'y':
-        getRanges()       
-    elif addMore[0].lower() == 'n':
-        tl.amtToScan(len(IP_list)) #Write number of IPs being scanned to run log
-        createdThreads = 0
-        threadSplit = len(IP_list) / 5
-
-        #Splitting the work up between the threads
-        thread_1_list = IP_list[0:threadSplit]
-        thread_2_list = IP_list[threadSplit:(threadSplit*2)]
-        thread_3_list = IP_list[(threadSplit*2):(threadSplit*3)]
-        thread_4_list = IP_list[(threadSplit*3):(threadSplit*4)]
-        thread_5_list = IP_list[(threadSplit*4):(threadSplit*5)]
-        thread_6_list = IP_list[(threadSplit*5):]
-
-        threadList = [] 
-        for address in range(threadSplit):
-            thread_1 = getCertInfo(thread_1_list[address])
-            thread_2 = getCertInfo(thread_2_list[address])
-            thread_3 = getCertInfo(thread_3_list[address])
-            thread_4 = getCertInfo(thread_4_list[address])
-            thread_5 = getCertInfo(thread_5_list[address])
-            thread_1.start()
-            thread_2.start()
-            thread_3.start()
-            thread_4.start()
-            thread_5.start()
-    
-            thread_1.join()      
-            thread_2.join()
-            thread_3.join()
-            thread_4.join()
-            thread_5.join()
-     
-            if address == threadSplit-1:
-                for address in range(len(thread_6_list)):
-                     thread_6 = getCertInfo(thread_6_list[address])
-                     thread_6.start()
-                     thread_6.join()
-            
-        CIH = certInfoHolder
-        openCSV = open(title, "a")
-        writeToCSV = csv.writer(openCSV)
-        for info in CIH:
-            writeToCSV.writerow([info[0], info[1], info[2], info[3], info[4] , info[5][16:],
-                                 info[6],info[7],info[8],0])
-        for info in brokenCerts:
-            writeToCSV.writerow([info[0],'','','','','','','','',info[1],info[2]])
-        openCSV.close()
-
                               
-def ipRange(start_ip, end_ip):
-    start = list(map(int, start_ip.split(".")))
-    end = list(map(int, end_ip.split(".")))
-    temp = start
-    
-    IP_list.append(start_ip)
-    while temp != end:
-        start[3] += 1
-        for i in (3, 2, 1):
-            if temp[i] == 256:
-                temp[i] = 0
-                temp[i-1] += 1        
-        IP = ".".join(map(str, temp))
-        IP_list.append(IP)
 
 class getCertInfo(threading.Thread):
-     def __init__(self,IP):
+     def __init__(self,IP,ssl_file):
          threading.Thread.__init__(self)
          self.IP_check=IP
+         self.ssl_file = ssl_file
 
      def run(self):
         try:
@@ -156,6 +169,7 @@ class getCertInfo(threading.Thread):
             elif re.search('60', self.err_code) is not None or re.search('51', self.err_code) is not None:
                 print 'Rechecking: '+str(self.IP_check)
                 self.getSSLCert()
+                return 
 
             elif re.match(' 7', self.eC) is not None:
                 self.x = [str(self.IP_check), '7', 'Failed to connect to host/proxy']
@@ -164,7 +178,9 @@ class getCertInfo(threading.Thread):
                 self.x = [str(self.IP_check), '35', 'Problem with SSL/TLS handshake']
             else:
                 self.x = [str(self.IP_check), self.err_code, '']
-            brokenCerts.append(self.x)                  
+            
+            print self.x[0]
+            self.ssl_file.writeToCSV(self.x,True)                
 
      def getSSLCert(self):
 
@@ -200,8 +216,8 @@ class getCertInfo(threading.Thread):
         self.x = [str(self.IP_check),str(self.companyName),str(self.formatDT.strftime("%Y-%m-%d")),
                   str(self.expiresIn.days), str(self.commonName) , str(self.issuer),
                   str(self.serialNumber),str(self.pubKey),str(self.pubKeySize)]
-        certInfoHolder.append(self.x)
-        print self.x
+
+        self.ssl_file.writeToCSV(self.x,False)
+        print self.x[0]
     
 main()
-
